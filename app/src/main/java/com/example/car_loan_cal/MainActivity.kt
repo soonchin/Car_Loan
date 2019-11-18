c0c3e916f6552bfcb40ea9adc01b9d91ae15c575
taruc <ersc-jm17@student.tarc.edu.my>
2019-11-18 15:09:05 +0800

    update

--- app/src/main/java/com/example/car_loan_cal/MainActivity.kt
+++ app/src/main/java/com/example/car_loan_cal/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCalculate.setOnClickListener(){
+            //bhbhbh
             calLoan()
         }
     }
